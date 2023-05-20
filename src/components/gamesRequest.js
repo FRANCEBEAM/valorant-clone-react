b7fcf633c69009ca3a95efb5858d8e6e47652e7d
FRANCEBEAM <santanderfrancisbeam@gmail.com>
2023-05-20 14:51:01 +0800

    first navbar

---
+++ src/components/gamesRequest.js
@@ -0,0 +1,83 @@
+
+
+export const Games = [
+    {
+        id: 1,
+        gameTitle: "assets/img/lollogo.svg",
+        gameImg: "assets/img/lol.jpeg",
+    },
+    {
+        id: 2,
+        gameTitle: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt73ba063c224de13c/620719f19c50e50a64ea776b/TFT_Evergreen_Logo_Bug-Wordmark-Stacked_Final.svg?",
+        gameImg: "assets/img/teamfight.jpeg",
+    },
+    {
+        id: 3,
+        gameTitle: "assets/img/valorantlogo.svg",
+        gameImg: "assets/img/valorant.jpeg",
+    },
+    {
+        id: 4,
+        gameTitle: "assets/img/runeterralogo.svg",
+        gameImg: "assets/img/runeterra.jpeg",
+    },
+    {
+        id: 5,
+        gameTitle: "assets/img/wildriftlogo.svg",
+        gameImg: "assets/img/wildrift.jpeg",
+    },
+]
+
+export const Forges = [
+    {
+        id: 1,
+        gameTitle: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt589f0c88c9f4fe5c/618d9795867d1817d95f3b15/HM.svg",
+        gameImg: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt8589cd81c00f5c6a/6189ee6d666f1777c9bcfa52/JFG_Key_Art_T2AV2.jpg??&format=pjpg&quality=85",
+    },
+    {
+        id: 2,
+        gameTitle: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt3b981873fd7d9af2/615a8521c34d415711f7e1da/Ruined_King.svg",
+        gameImg: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt160d084323f31a63/615a859c7ab23a526f225189/ruined_king.jpg??&format=pjpg&quality=85",
+    },
+    {
+        id: 3,
+        gameTitle: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt07a59d6098466b37/615a8d7df2882661476b6adb/convergence-logo.png",
+        gameImg: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt15a80d65a2698d1f/615a859d8f1f7b55821485ee/convergence.jpg??&format=pjpg&quality=85",
+    },
+    {
+        id: 4,
+        gameTitle: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt311c430e578b5709/615a85179c76234dd174c189/Riot_Forge.svg",
+        gameImg: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt8e6f343314f438ee/615d4fd6f2882661476b77da/Riot_Forge.jpg??&format=pjpg&quality=85",
+    },
+]
+
+export const Esports = [
+    {
+        id: 1,
+        espTitle: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt79e09e13810b1131/6424e722cc01de1087082bc5/033023_LoL_Logo_White.png",
+        espImg: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blta3add1f2553033a2/6424cbeb223f2f118c954ad5/033023_LoL_Esports_MegaMenu_Product_Card.jpg??&format=pjpg&quality=85",
+    },
+    {
+        id: 2,
+        espTitle: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt897524cd0f07ccbd/6424e722167b14113bedf6b8/033023_VCT23_Logo_White.png",
+        espImg: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt42e1162ed54d15d2/6424cbeb18bad210b77f86b7/033023_VCT_Esports_MegaMenu_Product_Card.jpg??&format=pjpg&quality=85",
+    },
+]
+
+export const Entertainments = [
+    {
+        id: 1,
+        enterTitle: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/bltd307b99a6713cbe9/615a8517a4d10c511f1ad399/arcane.svg",
+        enterImg: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blta84d6d394801dedb/615a859d9c76234dd174c195/arcane.jpg??&format=pjpg&quality=85",
+    },
+    {
+        id: 2,
+        enterTitle: "UNIVERSE",
+        enterImg: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/bltea74e94e1296b057/615a8fccc22cd45706cf29fe/universe.jpg??&format=pjpg&quality=85",
+    },
+    {
+        id: 3,
+        enterTitle: "RIOT GAMES MUSIC",
+        enterImg: "https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt4303739f818a50e3/615a859d054fcd4c567205ab/Riot_Music.jpg??&format=pjpg&quality=85",
+    },
+]
